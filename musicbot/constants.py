a7caf60824499b17525e957ea211304b08c26c91
Dodek69 <dodek@vip.interia.pl>
2019-02-04 21:21:46 +0100

    Initiation.

---
+++ musicbot/constants.py
@@ -0,0 +1,8 @@
+import os.path
+
+MAIN_VERSION = '0.9.9'
+SUB_VERSION = '_69'
+VERSION = MAIN_VERSION + SUB_VERSION
+
+AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
+DISCORD_MSG_CHAR_LIMIT = 2000
---
+++ update.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+
+def y_n(q):
+    while True:
+        ri = input('{} (y/n): '.format(q))
+        if ri.lower() in ['yes', 'y']: return True
+        elif ri.lower() in ['no', 'n']: return False
+
+def update_deps():
+    print("Attempting to update dependencies...")
+
+    try:
+        subprocess.check_call('"{}" -m pip install -U -r requirements.txt'.format(sys.executable), shell=True)
+    except subprocess.CalledProcessError:
+        raise OSError("Could not update dependencies. You will need to run '\"{0}\" -m pip install -U -r requirements.txt' yourself.".format(sys.executable))
+
+def finalize():
+    try:
+        from musicbot.constants import VERSION
+        print('The current Bot version is {0}.'.format(VERSION))
+    except Exception:
+        print('There was a problem fetching your current bot version. The installation may not have completed correctly.')
+
+    print("Done!")
+
+def main():
+    print('Starting...')
+
+    update_deps()
+    finalize()
+
+if __name__ == '__main__':
+    main()
